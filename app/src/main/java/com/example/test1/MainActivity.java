3550993de647f92876c461184955e862a63a417b
TOSHIBA <ushanjayasooriya@gmail.com>
2020-07-25 08:20:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.test1;
+
+import  androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "ushan";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
